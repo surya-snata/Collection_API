1eebcf44a2586f624b1a74f3b75e7cdf57c51485
suryasnata <suryasnatamohanty8594@gmailcom>
2024-11-11 23:39:24 +0530

    New API

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.get("/my-api/data", (req, res) => {
   res.status(200).send(data);
 });
 
+
+
 const port = process.env.PORT || 8181;
 app.listen(port, () => {
   console.log(`Server is running at port ${port}`);
